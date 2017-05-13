ee2ab6fb4f7fe15e8bc3f954c9313c23ead4ecc1
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2017-05-12 22:00:47 -0300

    Borrado de comentarios innecesarios.

--- TP1 Recu/src/ejercicio1/Main.java
+++ TP1 Recu/src/ejercicio1/Main.java
@@ -3,36 +3,20 @@ package ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-
-//		CashFlow c1 = new CashFlow();
-//		
-//		c1.agregarRegistro(new Registro(-100));
-//		c1.agregarRegistro(new Registro(100));
-//		c1.agregarRegistro(new Registro(-100));
-//		c1.agregarRegistro(new Registro(100));
-//		
-//		System.out.println("C1 original: " + c1);
-//		
-//		c1.obtenerSaldoPositivoSwapeandoPocasVeces();
-//		
-//		System.out.println("C1 Swapeado: " + c1);
 		
+		CashFlow c1 = new CashFlow();
 		
+		c1.agregarRegistro(new Registro(-100));
+		c1.agregarRegistro(new Registro(-100));
+		c1.agregarRegistro(new Registro(-100));
+		c1.agregarRegistro(new Registro(-100));
+		c1.agregarRegistro(new Registro(1000));
 		
+		System.out.println("C2 original: " + c1);
 		
-		CashFlow c2 = new CashFlow();
+		c1.obtenerSaldoPositivoSwapeandoPocasVeces();
 		
-		c2.agregarRegistro(new Registro(-100));
-		c2.agregarRegistro(new Registro(-100));
-		c2.agregarRegistro(new Registro(-100));
-		c2.agregarRegistro(new Registro(-100));
-		c2.agregarRegistro(new Registro(1000));
-		
-		System.out.println("C2 original: " + c2);
-		
-		c2.obtenerSaldoPositivoSwapeandoPocasVeces();
-		
-		System.out.println("C2 Swapeado: " + c2);
+		System.out.println("C2 Swapeado: " + c1);
 
 	}
 
