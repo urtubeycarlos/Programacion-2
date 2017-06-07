861146c28e9b4fc9fbedfa7bb0222e0479cd0014
Urtubey Carlos <urtubeycarlos.0510@gmail.com>
2017-06-07 12:14:58 -0300

    Definida la clase Palabras.

---
+++ TP2/src/prog2/tp2_2017a/Palabras.java
@@ -0,0 +1,17 @@
+package prog2.tp2_2017a;
+
+public class Palabras implements Alfabeto<Character> {
+
+	@Override
+	public int tam() {
+		return 26;
+	}
+
+	@Override
+	public int indice(Character c) {
+		if (c >= 'a' && c <= 'z')
+			return c - 'a';
+		throw new RuntimeException("Caracter no válido: " + c);
+	}
+
+}
